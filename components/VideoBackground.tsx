c6f4abd8e4fb750d6c5097c5e8d0d1d17500d0c3
TrendsAI-bit <trendsai.bit@gmail.com>
2025-06-13 21:55:26 +0800

    fix: Use CDN video for background

--- components/VideoBackground.tsx
+++ components/VideoBackground.tsx
@@ -10,7 +10,7 @@ export const VideoBackground = () => {
         playsInline
         className="w-full h-full object-cover"
       >
-        <source src="/fire.mp4" type="video/mp4" />
+        <source src="https://cdn.pixabay.com/video/2020/02/03/31086-388506591_large.mp4" type="video/mp4" />
       </video>
       <div className="absolute top-0 left-0 w-full h-full bg-black/50"></div>
     </div>
